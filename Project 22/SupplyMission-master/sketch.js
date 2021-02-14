84a4f1e3601b00623c6a9bf83e274b3bc21da1f4
Sriram26231 <74018096+Sriram26231@users.noreply.github.com>
2021-02-14 16:19:53 +0000

    Add files via upload

--- Project 22/SupplyMission-master/sketch.js
+++ Project 22/SupplyMission-master/sketch.js
@@ -23,18 +23,19 @@ function setup() {
 	helicopterSprite=createSprite(20, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6;
-	helicopterSprite.velocityX = 5;
+
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
+	helicopterSprite.velocityX = 5;
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(helicopterSprite.x , helicopterSprite.y , 5 , {restitution:1, isStatic:true});
-	World.add(world, packageBody);
 	
+	  //packageBody = Bodies.circle(helicopterSprite.x , helicopterSprite.y , 5 , {restitution:1, isStatic:true});
+
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
@@ -49,23 +50,20 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
+  //World.add(world, packageBody);
+  //packageSprite.x= packageBody.position.x 
+  //packageSprite.y= packageBody.position.y 
 
-  keyPressed();
+
+  
+
+	if (helicopterSprite.x = 400) {
+		//Matter.Body.setStatic(packageBody, false)
+
+
+		
  
 }
+drawSprites();
 
-function keyPressed() {
- if (helicopterSprite.x = 200) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:false});
-	World.add(world, packageBody);
-	
-    
-  }
 }
-
-
-
